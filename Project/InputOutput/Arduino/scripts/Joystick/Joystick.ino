f7f0e8ab09d0cb5abe0aeb32f1e3b5e272070781
MSI\user <semmyking@gmail.com>
2019-06-07 10:41:09 +0200

    update 2nd joystick control
    now 2 joysticks work at the same time

--- Project/InputOutput/Arduino/scripts/Joystick/Joystick.ino
+++ Project/InputOutput/Arduino/scripts/Joystick/Joystick.ino
@@ -14,17 +14,26 @@
   * copyleft 2005 DojoDave for DojoCorp
   */
 
-
+ const int buttonPin = 2; 
+ int buttonState = 0;  
  
  int analogPin1 = 0;
  int analogPin2 = 1;
+ int analogPin3 = 2;
+ int analogPin4 = 3;
+
  int val1 = 0;
  int val2 = 0;
+ int val3 = 0;
+ int val4 = 0;
 
 
  void setup() {
   pinMode(analogPin1, INPUT);
   pinMode(analogPin2, INPUT);
+  pinMode(analogPin3, INPUT);
+  pinMode(analogPin4, INPUT);
+  pinMode(buttonPin, INPUT);
   Serial.begin(9600);
  }
 
@@ -47,11 +56,19 @@
 	 return ((_x > 0) - (_x < 0)) || +_x;
  }
 
-
-
  void loop() {
+  buttonState = digitalRead(buttonPin);
 	val1 = analogRead(analogPin1);
-    val2 = analogRead(analogPin2);
-  Serial.println("X: " + String(round(val1/100)) + " | Y: " + String(val2/100));
+  val2 = analogRead(analogPin2);
+  val3 = analogRead(analogPin3);
+  val4 = analogRead(analogPin4);
+  Serial.println("X: " + String(round(val1/100)) + " | Y: " + String(val2/100)+"        |  X: " + String(round(val3/100)) + " | Y: " + String(val4/100));
+  delay(100);
+  
+  if (buttonState == HIGH) {
+    Serial.println("Button pressed");
+  }else {
+    Serial.println("Button not pressed");
+  }
+  
  }
-
