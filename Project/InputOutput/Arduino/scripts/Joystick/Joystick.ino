43edea3fbc001320e4518d5b1d7c52e995f9094b
MSI\user <semmyking@gmail.com>
2019-06-11 14:59:36 +0200

    update Joystick.ino
    update 2 working parallax joysticks plus working button.
    all three of them have debug reactions, still need functionalitty for in the game.

--- Project/InputOutput/Arduino/scripts/Joystick/Joystick.ino
+++ Project/InputOutput/Arduino/scripts/Joystick/Joystick.ino
@@ -1,19 +1,3 @@
- /* Read Jostick
-  * ------------
-  *
-  * Reads two analog pins that are supposed to be
-  * connected to a jostick made of two potentiometers
-  *
-  * We send three bytes back to the comp: one header and two
-  * with data as signed bytes, this will take the form:
-  *     Jxy\r\n
-  *
-  * x and y are integers and sent in ASCII 
-  * 
-  * http://www.0j0.org | http://arduino.berlios.de
-  * copyleft 2005 DojoDave for DojoCorp
-  */
-
  const int buttonPin = 2; 
  int buttonState = 0;  
  
@@ -27,8 +11,8 @@
  int val3 = 0;
  int val4 = 0;
 
-
  void setup() {
+  // inputs
   pinMode(analogPin1, INPUT);
   pinMode(analogPin2, INPUT);
   pinMode(analogPin3, INPUT);
@@ -37,6 +21,7 @@
   Serial.begin(9600);
  }
 
+// Math: sign function
  int signMid(int _value, int _negative, int _mid, int _positive)
  {
    int _return = 0;
@@ -57,18 +42,22 @@
  }
 
  void loop() {
+  // set input
   buttonState = digitalRead(buttonPin);
 	val1 = analogRead(analogPin1);
   val2 = analogRead(analogPin2);
   val3 = analogRead(analogPin3);
   val4 = analogRead(analogPin4);
+  // print joystick values
   Serial.println("X: " + String(round(val1/100)) + " | Y: " + String(val2/100)+"        |  X: " + String(round(val3/100)) + " | Y: " + String(val4/100));
   delay(100);
-  
+  // print button values
   if (buttonState == HIGH) {
     Serial.println("Button pressed");
+    delay(100);
   }else {
     Serial.println("Button not pressed");
+    delay(100);
   }
   
  }
